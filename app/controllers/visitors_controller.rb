258817dd91776cebb01700f896fb469b05f9075a
Grey Elerson <grey.l.elerson@gmail.com>
2014-09-23 01:40:55 -0500

    front end framework (foundation4) and simple_forms

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,8 @@ class VisitorsController < ApplicationController
   def new
     @owner = Owner.new
     #render 'visitors/new' # this code is redundant, but shows how the relationship works
-    flash.now[:notice] = "Welcome!"
-    flash.now[:alert] = "My birthday is soon!"
+    #flash.now[:notice] = "Welcome!"
+    #flash.now[:alert] = "My birthday is soon!"
   end
   
 end 
