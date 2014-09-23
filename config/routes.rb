6c0821540af01ebef972fb32f105b3258f0b51df
Grey Elerson <grey.l.elerson@gmail.com>
2014-09-23 15:06:58 -0500

    add 'about' and 'contact' pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#new'
+  
+  # get '/pages/*id', to: 'pages#show' #{redundant code because the high_voltage gem routes by default}
+
+  # resources :posts # this format will generate the INDUCES routes, so they can be accessed bu _path helper methods
 end
