522cf586f97b5c01afa3e8c7087f7d1e4c189aae
Grey Elerson <grey.l.elerson@gmail.com>
2014-09-23 00:22:00 -0500

    update application layout to make use of rails_layout gem

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,9 @@ class VisitorsController < ApplicationController
   
   def new
     @owner = Owner.new
-    render 'visitors/new' # this code is redundant, but shows how the relationship works
+    #render 'visitors/new' # this code is redundant, but shows how the relationship works
+    flash.now[:notice] = "Welcome!"
+    flash.now[:alert] = "My birthday is soon!"
   end
   
 end 
