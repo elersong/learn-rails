8ec2337e308e97dcb3df10109133422d5824d19b
Grey Elerson <grey.l.elerson@gmail.com>
2014-09-23 20:17:45 -0500

    SimpleForm contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#new'
+  resources :contacts, only: [:new, :create]
   
   # get '/pages/*id', to: 'pages#show' #{redundant code because the high_voltage gem routes by default}
 
